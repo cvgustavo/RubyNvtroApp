b673d45326a861f5743524c073d856103e14a49c
cvgustavo <cvgustavo@gmail.com>
2014-09-15 05:25:11 -0300

    Created Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NvtroApp::Application.config.secret_key_base = '82781b1c312d4f504bcaa0e1b22ef57294a689a0a736275c84bde664c0ee1d2d68d5f744820a3a983ed7f6a1d4502054b5f4a329d29bf0cabae6e9e7b1c660fd'
