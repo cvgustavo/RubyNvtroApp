6bc9e2cef8d9e890edcd17bfec07192d1b929891
cvgustavo <cvgustavo@gmail.com>
2014-09-15 20:13:22 -0300

    Add Scaffold Paciente

---
+++ app/views/pacientes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @paciente, :id, :nome, :convenio_id, :carteiranumero, :idade, :peso, :altura, :iskid, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 NvtroApp::Application.routes.draw do
+  resources :pacientes
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
