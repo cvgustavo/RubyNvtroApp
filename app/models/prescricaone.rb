1082a26e81a3fbd448fb8982f372c9b0e7d740eb
cvgustavo <cvgustavo@gmail.com>
2014-09-18 11:15:42 -0300

    Added Attachments to PrescricaoNe Model

--- app/models/prescricaone.rb
+++ app/models/prescricaone.rb
@@ -1,4 +1,16 @@
 class Prescricaone < ActiveRecord::Base
-  belongs_to :paciente
-  belongs_to :internacao
+	has_attached_file :autorizacao, :styles => { :dpi300 => "2480x3508>", :dpi150 => "1240x1754>" }, :default_url => "/images/:style/missing.png"
+	validates_attachment :autorizacao,  :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png","application/pdf"] },
+	:size => { :in => 0..2.megabytes }
+	has_attached_file :contamedica, :styles => { :dpi300 => "2480x3508>", :dpi150 => "1240x1754>" }, :default_url => "/images/:style/missing.png"
+	validates_attachment :contamedica,  :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png","application/pdf"] },
+	:size => { :in => 0..2.megabytes }
+	has_attached_file :checagem, :styles => { :dpi300 => "2480x3508>", :dpi150 => "1240x1754>" }, :default_url => "/images/:style/missing.png"
+	validates_attachment :checagem,  :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png","application/pdf"] },
+	:size => { :in => 0..2.megabytes }
+	has_attached_file :entrega, :styles => { :dpi300 => "2480x3508>", :dpi150 => "1240x1754>" }, :default_url => "/images/:style/missing.png"
+	validates_attachment :entrega,  :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png","application/pdf"] },
+	:size => { :in => 0..2.megabytes }
+	belongs_to :paciente
+	belongs_to :internacao
 end
